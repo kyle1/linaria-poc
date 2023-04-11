389ab93f32202c4b92d5c90b1e9c92039ddc7c87
Kyle Overstreet <koverstreet1@csub.edu>
2023-04-11 13:32:25 -0700

    WIP counter page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// References for Nextjs + Linaria:
+// https://github.com/dlehmhus/next-with-linaria
+// https://focusreactive.com/nextjs-with-linaria-storybook/
+const withLinaria = require("next-with-linaria");
 
-module.exports = nextConfig
+/** @type {import('next-with-linaria').LinariaConfig} */
+const config = {
+  experimental: {
+    appDir: true,
+  },
+};
+module.exports = withLinaria(config);
---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+import { styled } from "@linaria/react";
+
+export const Button = styled.button<{ color: string }>`
+  border: 1px solid ${({ color }) => color};
+  border-radius: 5px;
+  color: white;
+  background-color: ${({ color }) => color};
+  width: 100px;
+  height: 50px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #05485a;
+  }
+
+  &:active {
+    transform: translateY(2px);
+  }
+`;
