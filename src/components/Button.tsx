566fa13cbf9a5e75349c8cee770da1ddcb9d74f1
Kyle Overstreet <koverstreet1@csub.edu>
2023-04-17 12:55:47 -0700

    WIP theming

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,18 +5,13 @@ import { styled } from "@linaria/react";
 /* border: 1px solid ${({ color }) => color}; */
 
 export const Button = styled.button`
+  width: 100px;
+  height: 50px;
   border: 1px solid var(--color-primary);
   border-radius: 5px;
   background-color: var(--color-primary);
   color: white;
-  width: 100px;
-  height: 50px;
   cursor: pointer;
-  /* 
-  &:hover {
-    border: 1px solid #05485a;
-    background-color: #05485a;
-  } */
 
   &:hover {
     border: 1px solid var(--color-button-primary-background-hover);
@@ -24,7 +19,6 @@ export const Button = styled.button`
   }
 
   &:active {
-    transform: translateY(2px);
     border: 1px solid var(--color-button-primary-background-active);
     background-color: var(--color-button-primary-background-active);
   }
--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -3,14 +3,16 @@ import { css } from "@linaria/core";
 export const lightMode = css`
   --color-primary-base: 220, 80%;
   --color-secondary-base: 208, 7%;
+  --color-border-base: 222, 33%;
 
   --color-background: hsl(0, 0%, 100%);
   --color-text: hsl(0, 0%, 0%);
-  --color-primary: hsl(var(--color-primary-base), 39%);
+  --color-border: hsl(var(--color-border-base), 94%);
+  --color-primary: hsl(var(--color-primary-base), 42%);
   --color-secondary: hsl(var(--color-secondary-base), 46%);
 
-  --color-button-primary-background-hover: hsl(var(--color-primary-base), 33%);
-  --color-button-primary-background-active: hsl(var(--color-primary-base), 27%);
+  --color-button-primary-background-hover: hsl(var(--color-primary-base), 36%);
+  --color-button-primary-background-active: hsl(var(--color-primary-base), 30%);
 
   background-color: var(--color-background);
   color: var(--color-text);
@@ -19,9 +21,11 @@ export const lightMode = css`
 export const darkMode = css`
   --color-primary-base: 204, 87.6%;
   --color-secondary-base: 208, 7%;
+  --color-border-base: 224, 15%;
 
   --color-background: hsl(0, 0%, 0%);
   --color-text: hsla(0, 0%, 100%, 87%);
+  --color-border: hsl(var(--color-border-base), 19%);
   --color-primary: hsl(var(--color-primary-base), 52.7%);
   --color-secondary: hsl(var(--color-secondary-base), 46%);
 
